d0bb6e564997feba01f93c2a1341353e81876baa
coldDelight <hno05039@naver.com>
2024-02-22 22:47:57 +0900

    [Setting] 폰트 적용

--- core/designsystem/src/main/java/com/coldblue/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/coldblue/designsystem/theme/Theme.kt
@@ -64,7 +64,7 @@ fun HarumandalartTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = typography,
         content = content
     )
 }
--- core/designsystem/src/main/java/com/coldblue/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/com/coldblue/designsystem/theme/Type.kt
@@ -2,33 +2,65 @@ package com.coldblue.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.coldblue.designsystem.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+val orbit = FontFamily(
+    Font(R.font.orbit)
 )
+
+val typography = Typography(
+    headlineLarge = TextStyle(fontFamily = orbit),
+    headlineMedium = TextStyle(fontFamily = orbit),
+    headlineSmall = TextStyle(fontFamily = orbit),
+    bodyLarge = TextStyle(fontFamily = orbit),
+    bodyMedium = TextStyle(fontFamily = orbit),
+    bodySmall = TextStyle(fontFamily = orbit),
+    labelLarge = TextStyle(fontFamily = orbit),
+    labelMedium = TextStyle(fontFamily = orbit),
+    labelSmall = TextStyle(fontFamily = orbit),
+    titleLarge = TextStyle(fontFamily = orbit),
+    titleMedium = TextStyle(fontFamily = orbit),
+    titleSmall = TextStyle(fontFamily = orbit),
+    displayLarge = TextStyle(fontFamily = orbit),
+    displayMedium = TextStyle(fontFamily = orbit),
+    displaySmall = TextStyle(fontFamily = orbit),
+)
+
+object HmStyle{
+    val logo = TextStyle(
+        fontFamily = orbit,
+        fontSize = 30.sp,
+    )
+    val headline = TextStyle(
+        fontFamily = orbit,
+        fontSize = 20.sp,
+    )
+    val title = TextStyle(
+        fontFamily = orbit,
+        fontSize = 16.sp,
+    )
+    val content = TextStyle(
+        fontFamily = orbit,
+        fontSize = 12.sp,
+    )
+    val detail = TextStyle(
+        fontFamily = orbit,
+        fontSize = 8.sp,
+    )
+    val small = TextStyle(
+        fontFamily = orbit,
+        fontSize = 6.sp,
+    )
+
+    val tiny = TextStyle(
+        fontFamily = orbit,
+        fontSize = 4.sp,
+    )
+
+}
+
+
+
