b829a27b8dd402442e9286eda2ca7cb460fd3cac
pureum <lpm0831@naver.com>
2024-02-25 15:43:39 +0900

    [Feature] Manda - Data Store 로직 구현

--- core/datastore/src/main/java/com/coldblue/datastore/UserDataSource.kt
+++ core/datastore/src/main/java/com/coldblue/datastore/UserDataSource.kt
@@ -8,10 +8,12 @@ interface UserDataSource {
     val mandaUpdateTime: Flow<String>
     val isTutorial: Flow<Boolean>
     val isAlarm: Flow<Boolean>
+    val isInit: Flow<Boolean>
 
     suspend fun updateToken(token: String)
     suspend fun updateTodoTime(time: String)
     suspend fun updateMandaTime(time: String)
     suspend fun updateTutorial(state: Boolean)
     suspend fun updateAlarm(state: Boolean)
+    suspend fun updateInit(state: Boolean)
 }
