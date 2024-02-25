ba8cddabd90c640f37e74c8fa3535bb8b5a793ab
coldDelight <hno05039@naver.com>
2024-02-26 02:07:11 +0900

    [Feature] Todo dao 수정

--- core/database/src/main/java/com/coldblue/database/dao/TodoDao.kt
+++ core/database/src/main/java/com/coldblue/database/dao/TodoDao.kt
@@ -4,16 +4,19 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.coldblue.database.entity.MandaEntity
 import com.coldblue.database.entity.TodoEntity
 import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
 
 @Dao
 interface TodoDao {
-    @Query("Select * From todo")
-    fun getTodoEntities(): Flow<List<TodoEntity>>
+    @Query("Select * From todo WHERE date=:date")
+    fun getTodo(date: LocalDate): Flow<List<TodoEntity>>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun upsertTodoEntities(todoEntities: List<TodoEntity>)
-    @Query("Delete From todo")
-    suspend fun deleteTodoEntities()
+    suspend fun upsertTodo(todo: TodoEntity)
+
+    @Query("Delete From todo WHERE id = :todoId")
+    suspend fun deleteTodo(todoId: Int)
+
 }
