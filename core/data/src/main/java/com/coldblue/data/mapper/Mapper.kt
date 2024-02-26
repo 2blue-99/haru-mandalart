f474a7b33b246d5dcf647113aa4ba6d17353e446
pureum <lpm0831@naver.com>
2024-02-26 14:19:18 +0900

    [Bug] Git Test

--- core/data/src/main/java/com/coldblue/data/mapper/Mapper.kt
+++ core/data/src/main/java/com/coldblue/data/mapper/Mapper.kt
@@ -1,50 +1,16 @@
 package com.coldblue.data.mapper
 
-import com.coldblue.database.entity.MandaDetailEntity
-import com.coldblue.database.entity.MandaKeyEntity
+import com.coldblue.database.entity.MandaEntity
 import com.coldblue.database.entity.TodoEntity
-import com.coldblue.model.MandaDetail
-import com.coldblue.model.MandaKey
+import com.coldblue.model.Manda
 import com.coldblue.model.Todo
 
 object Mapper {
-    fun MandaKeyEntity.asMandaKeyModel(): MandaKey = MandaKey(
-        id = id,
-        name = name,
-        colorIndex = colorIndex
-    )
+    fun MandaEntity.asManda(): Manda = Manda(id, isSync, isDel, updateTime, name, color)
 
-    fun MandaKey.asMandaKeyEntity(): MandaKeyEntity = MandaKeyEntity(
-        id = id,
-        isSync = false,
-        isDel = false,
-        updateTime = "updateTime",
-        name = name,
-        colorIndex = colorIndex
-    )
+    fun Manda.asMandaEntity(): MandaEntity = MandaEntity(id, isSync, isDel, updateTime, name, color)
 
+//    fun TodoEntity.asTodo(): Todo = Todo(id, originId, haruMandaId, isSync, isDel, updateTime, title, content, time)
 
-    fun MandaDetailEntity.asMandaDetailModel(): MandaDetail = MandaDetail(
-        id = id,
-        mandaId = mandaId,
-        name = name,
-        isDone = isDone
-    )
-
-    fun MandaDetail.asMandaDetailEntity(): MandaDetailEntity = MandaDetailEntity(
-        id = id,
-        isSync = false,
-        isDel = false,
-        updateTime = "updateTime",
-        mandaId = mandaId,
-        name = name,
-        isDone = isDone
-    )
-
-
-    fun TodoEntity.asTodo(): Todo =
-        Todo(id, originId, haruMandaId, isSync, isDel, updateTime, title, content, time)
-
-    fun Todo.asTodoEntity(): TodoEntity =
-        TodoEntity(id, originId, harumandaId, isSync, isDel, updateTime, title, content, time)
+//    fun Todo.asTodoEntity(): TodoEntity = TodoEntity(id, originId, harumandaId, isSync, isDel, updateTime, title, content, time)
 }
