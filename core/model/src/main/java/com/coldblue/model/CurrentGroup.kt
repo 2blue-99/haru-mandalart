1669138de44404d61fa26c44048e6e78cc02def0
coldDelight <hno05039@naver.com>
2024-02-26 11:18:48 +0900

    [Feature] model 기본값 추가 - CurrentGroup - Todo - TodoGroup

--- core/model/src/main/java/com/coldblue/model/CurrentGroup.kt
+++ core/model/src/main/java/com/coldblue/model/CurrentGroup.kt
@@ -1,10 +1,20 @@
 package com.coldblue.model
 
+import java.time.LocalDate
+
 data class CurrentGroup(
     val todoGroupId: Int,
-    val name: String,
-    val isSync: Boolean,
-    val isDel: Boolean,
-    val updateTime: String,
+    val name: String="",
+    val isSync: Boolean = false,
+    val isDel: Boolean = false,
+    val updateTime: String = LocalDate.now().toString(),
     val id: Int = 0,
 )
+
+data class CurrentGroupWithCnt(
+    val currentGroup: CurrentGroup,
+    val doneTodoCnt: Int
+)
+
+fun CurrentGroup.withCnt(doneTodoCnt: Int): CurrentGroupWithCnt =
+    CurrentGroupWithCnt(this, doneTodoCnt = doneTodoCnt)
