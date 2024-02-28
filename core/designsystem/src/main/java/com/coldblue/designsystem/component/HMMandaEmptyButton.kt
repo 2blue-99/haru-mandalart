8b6d80711e49057d8f20bba434c092f60f03a80c
pureum <lpm0831@naver.com>
2024-02-28 17:38:41 +0900

    [Feature] Manda - Button

---
+++ core/designsystem/src/main/java/com/coldblue/designsystem/component/HMMandaEmptyButton.kt
@@ -0,0 +1,77 @@
+package com.coldblue.designsystem.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.coldblue.designsystem.IconPack
+import com.coldblue.designsystem.iconpack.Plus
+import com.coldblue.designsystem.theme.HMColor
+
+@Composable
+fun HMMandaEmptyButton() {
+    val stroke = Stroke(
+        width = 20f,
+        pathEffect = PathEffect.dashPathEffect(intervals = floatArrayOf(40f, 40f), phase = 0f)
+    )
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1F)
+            .padding(vertical = 5.dp, horizontal = 5.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .drawBehind {
+                drawRoundRect(
+                    color = Color.Gray,
+                    style = stroke,
+                    cornerRadius = CornerRadius(3.dp.toPx())
+                )
+            }
+    ){
+        Icon(
+            modifier = Modifier.padding(100.dp).fillMaxSize(),
+            imageVector = IconPack.Plus,
+            tint = HMColor.Gray,
+            contentDescription = ""
+        )
+    }
+//    Button(
+//        shape = RectangleShape,
+//        colors = ButtonDefaults.outlinedButtonColors(containerColor = Color.LightGray),
+//        onClick = { /*TODO*/ },
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .aspectRatio(1F)
+//            .clip(RoundedCornerShape(8.dp))
+//            .drawBehind {
+//                drawRoundRect(
+//                    color = Color.LightGray,
+//                    style = stroke,
+//                    cornerRadius = CornerRadius(30.dp.toPx())
+//                )
+//            },
+//    ) {
+//
+//    }
+}
+
+@Preview
+@Composable
+fun HMMandaEmptyPreview() {
+    HMMandaEmptyButton()
+}
---
+++ feature/manda/src/main/java/com/coldblue/mandalart/content/UnInitializedMandaContent.kt
@@ -0,0 +1,89 @@
+package com.coldblue.mandalart.content
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.coldblue.designsystem.component.HMButton
+import com.coldblue.designsystem.component.HMChip
+import com.coldblue.designsystem.component.HMTextField
+import com.coldblue.designsystem.theme.HmStyle
+import com.coldblue.mandalart.util.getTagList
+
+@Composable
+@OptIn(ExperimentalLayoutApi::class)
+fun UnInitializedMandaContent(
+    updateInitState: (Boolean) -> Unit,
+    insertFinalManda: (String) -> Unit
+) {
+    var inputText by remember { mutableStateOf("") }
+    var buttonClickableState by remember { mutableStateOf(false) }
+
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        item {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "당신의 최종 목표는 \n무엇인가요?",
+                style = HmStyle.text24,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Left
+            )
+        }
+        item {
+            HMTextField(inputText) {
+                inputText = it
+                buttonClickableState = it.isNotBlank()
+            }
+        }
+        item {
+            FlowRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(15.dp),
+            ) {
+                getTagList().forEach {
+                    HMChip(it) {
+                        inputText = it
+                        buttonClickableState = true
+                    }
+                }
+            }
+        }
+        item { Spacer(modifier = Modifier.height(10.dp)) }
+
+        item {
+            HMButton(text = "목표 구체화 하기", buttonClickableState) {
+                updateInitState(true)
+                insertFinalManda(inputText)
+            }
+        }
+    }
+//        Box(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .padding(bottom = 50.dp),
+//            contentAlignment = Alignment.BottomCenter,
+//        ) {
+//            HMButton(text = "목표 구체화 하기",clickState) { onNextClick() }
+//        }
+}
