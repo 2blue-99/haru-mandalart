cf82ae8dad778319409ec076e6b06e672d726245
pureum <lpm0831@naver.com>
2024-03-05 17:09:03 +0900

    [Feature] DesignSystem - HMTextField 사용성 개선

--- core/designsystem/src/main/java/com/coldblue/designsystem/component/HMTextField.kt
+++ core/designsystem/src/main/java/com/coldblue/designsystem/component/HMTextField.kt
@@ -1,25 +1,35 @@
 package com.coldblue.designsystem.component
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.coldblue.designsystem.theme.HMColor
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HMTextField(
-    text: String = "",
+    inputText: String = "",
     onChangeText: (String) -> Unit
 ) {
+    var test by remember { mutableStateOf(inputText) }
+    LaunchedEffect(inputText){
+        test = inputText
+    }
     TextField(
         modifier = Modifier.fillMaxWidth(),
-        value = text,
-        onValueChange = { onChangeText(it) },
+        value = test,
+        onValueChange = {
+            test = it
+            onChangeText(test)
+        },
         colors = TextFieldDefaults.colors(
             focusedIndicatorColor = HMColor.Primary,
             unfocusedContainerColor = Color.Transparent,
@@ -31,5 +41,5 @@ fun HMTextField(
 @Preview
 @Composable
 fun Preview() {
-    HMTextField{}
+    HMTextField {}
 }
