ce45dea1e0f1b0476fc3b90409391a05d9731def
pureum <lpm0831@naver.com>
2024-08-06 23:54:33 +0900

    [Feature] Tutorial - Dim

--- feature/tutorial/src/main/java/com/coldblue/tutorial/TutorialScreen.kt
+++ feature/tutorial/src/main/java/com/coldblue/tutorial/TutorialScreen.kt
@@ -1,16 +1,22 @@
 package com.coldblue.tutorial
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import com.coldblue.designsystem.theme.HMColor
 
 @Composable
 fun TutorialScreen(navigateToManda: () -> Unit){
-    Column {
-        Text(text = "TutorialScreen")
-        Button(onClick = { navigateToManda() }) {
-            Text(text = "Navigate To Manda")
-        }
+    Surface(
+        modifier = Modifier.fillMaxSize().alpha(0.7f).background(Color.Black)
+    ) {
+
     }
 }
