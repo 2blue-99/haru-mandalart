945914b52115fdacd2eedd173b65dd8f123655a9
pureum <lpm0831@naver.com>
2024-08-21 23:07:34 +0900

    [Feature] Tutorial - Offset, Size setting

--- feature/manda/src/main/java/com/coldblue/mandalart/screen/content/InitializedMandaContent.kt
+++ feature/manda/src/main/java/com/coldblue/mandalart/screen/content/InitializedMandaContent.kt
@@ -1,6 +1,5 @@
 package com.coldblue.mandalart.screen.content
 
-import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
@@ -20,16 +19,16 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.shape.ZeroCornerSize
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -56,7 +55,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
@@ -64,6 +62,7 @@ import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
@@ -147,7 +146,6 @@ fun InitializedMandaContent(
         modifier = Modifier
             .fillMaxSize()
             .background(HMColor.Background),
-        contentAlignment = Alignment.Center
     ) {
         Column(
             modifier = Modifier
@@ -162,7 +160,8 @@ fun InitializedMandaContent(
 
             Box(
                 modifier = Modifier.onGloballyPositioned {
-                    it.positionInRoot()
+//                    offset = it.positionInRoot()
+//                    size = it.size
                 }
             ) {
                 ExplainBox {
@@ -186,9 +185,10 @@ fun InitializedMandaContent(
 
             Box(
                 modifier = Modifier.onGloballyPositioned {
-                    it.positionInRoot()
+                    offset = it.positionInRoot()
+                    size = it.size
                 }
-            ){
+            ) {
                 ExplainBox {
                     Mandalart(
                         mandaList = uiState.mandaList,
@@ -201,7 +201,8 @@ fun InitializedMandaContent(
 
             Box(
                 modifier = Modifier.onGloballyPositioned {
-                    it.positionInRoot()
+//                    offset = it.positionInRoot()
+//                    size = it.size
                 }
             ) {
                 ExplainBox {
@@ -225,6 +226,23 @@ fun InitializedMandaContent(
                     .clickable(false) {}
                     .alpha(0.5f)
                     .background(Color.Black)
+            )
+        }
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = Icons.Default.ThumbUp,
+                tint = HMColor.Background,
+                modifier = Modifier
+                    .offset {
+                        IntOffset(
+                            offset.x.toInt(),
+                            offset.y.toInt() + size.height
+                        )
+                    },
+                contentDescription = ""
             )
         }
     }
